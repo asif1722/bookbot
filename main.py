988e7237b293038cbc5d3d0dc9aaaba2c00f0797
asif1722 <asifantulay172@gmail.com>
2025-03-08 10:52:44 +0300

    word counter function

--- main.py
+++ main.py
@@ -1,12 +1,18 @@
+def main():
+    book = get_book_text()
+    count = word_count(book)
+    print(f"{count} words found in the document")
+    
 def get_book_text():
     path = input("Enter the filepath to the book: ")
     with open(path) as f:
         book_text = f.read()
         return book_text
-    
-def main():
-    book = get_book_text()
-    print(book)
-    
+
+def word_count(book):
+    word_list = book.split()
+    count = len(word_list)
+    return count
+  
 main()
         
