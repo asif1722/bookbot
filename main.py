04711a04a589ca3a4ee54a234afa35bd8819b346
asif1722 <asifantulay172@gmail.com>
2025-03-08 10:59:20 +0300

    created module

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+from stats import word_count
+
 def main():
     book = get_book_text()
     count = word_count(book)
@@ -8,11 +10,6 @@ def get_book_text():
     with open(path) as f:
         book_text = f.read()
         return book_text
-
-def word_count(book):
-    word_list = book.split()
-    count = len(word_list)
-    return count
   
 main()
         
---
+++ stats.py
@@ -0,0 +1,4 @@
+def word_count(book):
+    word_list = book.split()
+    count = len(word_list)
+    return count
