f3145292de52cb44ef142e0dbf06b5d438fb271a
asif1722 <asifantulay172@gmail.com>
2025-03-08 11:45:52 +0300

    added book report function

--- main.py
+++ main.py
@@ -1,20 +1,29 @@
 from stats import *
 
 def main():
-    book = get_book_text()
+    path, book = get_book_text()
     word_count = word_counter(book)
-    print(f"{word_count} words found in the document")
     char_count = char_counter(book)
-    print(char_count)
     char_dict_list = char_dict_lister(char_count)
-    print(char_dict_list)
+    book_report(path, word_count, char_dict_list)
     
     
 def get_book_text():
     path = input("Enter the filepath to the book: ")
     with open(path) as f:
         book_text = f.read()
-        return book_text
+        return path, book_text
+    
+def book_report(path, words, chars):
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {path}")
+    print("----------- Word Count ----------")
+    print(f"Found {words} total words")
+    print("--------- Character Count -------")
+    for c in chars:
+        if c['char'].isalpha():
+            print(f"{c['char']}:{c['count']}")
+    print("============= END ===============")
   
 main()
         
