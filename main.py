02ad78671cc9b0d4cb1695c0d7d30b1e9dbc7978
asif1722 <asifantulay172@gmail.com>
2025-03-08 11:14:36 +0300

    created char counter function

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
-from stats import word_count
+from stats import word_counter, char_counter
 
 def main():
     book = get_book_text()
-    count = word_count(book)
-    print(f"{count} words found in the document")
+    word_count = word_counter(book)
+    print(f"{word_count} words found in the document")
+    char_count = char_counter(book)
+    print(char_count)
+    
     
 def get_book_text():
     path = input("Enter the filepath to the book: ")
--- stats.py
+++ stats.py
@@ -1,4 +1,14 @@
-def word_count(book):
+def word_counter(book):
     word_list = book.split()
     count = len(word_list)
     return count
+
+def char_counter(book):
+    char_list = list(book.lower())
+    char_dict = {}
+    for char in char_list:
+        if char in char_dict:
+            char_dict[char] += 1
+        else:
+            char_dict[char] = 1
+    return char_dict
